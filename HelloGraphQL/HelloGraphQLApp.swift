ccad96ab38e2ad1b202c8275c149203d190daa61
Mohammad Azam <azamsharp@gmail.com>
2021-05-10 12:19:34 -0500

    Initial Commit

---
+++ HelloGraphQL/HelloGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloGraphQLApp.swift
+//  HelloGraphQL
+//
+//  Created by Mohammad Azam on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
